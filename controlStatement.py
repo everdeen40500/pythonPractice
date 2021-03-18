1377d085e25642523f4b6db31e161027a55f6206
chobora <everdeen40500@gmail.com>
2021-03-19 00:24:13 +0900

    added an example that I solved.

--- controlStatement.py
+++ controlStatement.py
@@ -54,7 +54,7 @@ while True:
 print(i)
 
 
-#continue문
+#continue문 - continue 만나면 그 아래 코드 실행하지 않고 바로 while의 조건문으로 점프한다.
 i = 0
 while i < 15: #i가 홀수면 이 부분으로 온다.
     i = i + 1
@@ -62,3 +62,95 @@ while i < 15: #i가 홀수면 이 부분으로 온다.
     if i % 2 == 1:
         continue #i가 홀수가 맞으면 실행하는 부분
     print(i)
+
+
+print(" ============= ")
+n = 1
+i = 1
+
+# while n < 10:
+#     while i < 10:
+#         print("{} * {} = {}".format(n, i, n*i))
+
+
+
+# 예제
+print(" == 예제 1 ==")
+def print_grade(midterm_score, final_score):
+    total = midterm_score + final_score
+    if total >= 90:
+        print("A")
+    elif total >= 80:
+        print("B")
+    elif total >= 70:
+        print("C")
+    elif total >= 60:
+        print("D")
+    else:
+        print("F")
+
+# 테스트
+print_grade(40, 45)
+print_grade(20, 35)
+print_grade(30, 32)
+print_grade(50, 45)
+
+
+print()
+print(" == 예제 2 == ")
+# 100 이하의 자연수 중 8의 배수이지만 12의 배수는 아닌 것을 모두 출력하세요.
+while i <= 100:
+    if ((i % 8) == 0) & ((i % 12) != 0):
+        print(i)
+    i += 1
+
+
+print()
+print(" == 예제 3 == ")
+# 1,000보다 작은 자연수 중 2 또는 3의 배수의 합을 출력하는 프로그램을 써 보세요.
+i = 2
+sum = 0
+while i < 1000:
+    if i % 2 == 0 or i % 3 == 0:
+        sum += i
+        # sum = i + sum
+    i += 1
+print(sum)
+
+
+print()
+print(" == 예제 4 == ")
+# 정수 120의 약수를 모두 출력하고, 총 몇개의 약수가 있는지 출력하는 프로그램을 써 보세요
+sum = 0
+i = 1
+
+while i <= 120:
+    if 120 % i == 0: #약수가 맞다면!!
+        print(i) #약수 출력
+        sum += 1 # sum = sum + 1
+    i += 1
+print("120의 약수는 총 {}개입니다.".format(sum))
+
+
+
+print()
+print(" == 예제 5 == ")
+money = 50000000 #5천만원 - 1988
+apartment1988 = 50000000
+aprtment2016 = 1100000000
+
+#2016 은행돈 = money
+#print(2016 - 1988)=28
+n = 1
+while n < 29:
+    money *= 1.12 #money = money * 1.12
+    n += 1
+
+moneywin = money - aprtment2016
+eunmiwin = aprtment2016 - money
+if money > aprtment2016:
+    print("{}원 차이로 동일 아저씨 말씀이 맞습니다.".format(int(moneywin)))
+elif aprtment2016 > money:
+    print("{}원 차이로 미란 아주머니 말씀이 맞습니다.".format(int(eunmiwin)))
+
+
